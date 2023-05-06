46acf2e359cc44ffb1e5de927002e615a0f7ab95
Alexander Denner <a.denner@oth-aw.de>
2023-05-06 09:48:51 +0200

    Anlegen des Testframeworks

---
+++ 03_Implementierung/quaKrypto/TestLibrary/Summator_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using quaKrypto.Models;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class Summator_UnitTest
+    {
+        [Test]
+        public void Add_Two_Numbers_Success()
+        {
+            //Arrange 
+            Summator summator = new Summator();
+            int x = 1;
+            int y = 2;
+
+            //Act
+            int result = summator.Add(x, y);
+
+            //Assert
+            int expected_result = 3;
+            Assert.AreEqual(expected_result, result);
+        }
+    }
+}
