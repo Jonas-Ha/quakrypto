c92c477afa6b674b640d2f8905d64979e05d5534
Alexander Denner <a.denner@oth-aw.de>
2023-05-10 18:06:45 +0200

    Anlegen des Projekts

---
+++ 03_Implementierung/quaKryptoAlt/quaKrypto/Models/Summator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quaKrypto.Models
+{
+    public class Summator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
--- 03_Implementierung/quaKryptoAlt/quaKrypto/Views/HauptMenu.xaml.cs
+++ 03_Implementierung/quaKryptoAlt/quaKrypto/Views/HauptMenu.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace quaKrypto
+namespace quaKrypto.Views
 {
     /// <summary>
     /// Interaction logic for HauptMenu.xaml
