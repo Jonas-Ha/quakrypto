53299a1d62d29f9ce526ff1153cf7b19e04f13a3
Dominik Mentel <dominik@mentel-web.de>
2023-05-27 15:29:38 +0200

    Anpassungen LobbyBeitritt

---
+++ 03_Implementierung/quaKrypto/quaKrypto/Models/Classes/UebungsszenarioNetzwerkBeitrittInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace quaKrypto.Models.Classes
+{
+    public class UebungsszenarioNetzwerkBeitrittInfo
+    {
+        public string Lobbyname { get; set; }
+        public string Variante { get; set; }
+        public string Schwierigkeitsgrad { get; set; }
+        public BitmapImage AliceIcon { get; set; }
+        public BitmapImage BobIcon { get; set; }
+        public BitmapImage EveIcon { get; set; }
+    }
+}
--- 03_Implementierung/quaKrypto/quaKrypto/ViewModels/LobbyBeitrittViewModel.cs
+++ 03_Implementierung/quaKrypto/quaKrypto/ViewModels/LobbyBeitrittViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using quaKrypto.Commands;
+using quaKrypto.Models.Classes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,7 @@ namespace quaKrypto.ViewModels
     {
         public DelegateCommand HauptMenu { get; set; }
         public DelegateCommand LobbyBeitreten { get; set; }
-
+        public ObservableCollection<UebungsszenarioNetzwerkBeitrittInfo> LobbyInformation { get; set; }
         public LobbyBeitrittViewModel(Navigator navigator)
         {
 
@@ -23,9 +25,14 @@ namespace quaKrypto.ViewModels
 
             LobbyBeitreten = new((o) =>
             {
-                //navigator.aktuellesViewModel = new LobbyScreenViewModel(navigator);
+                navigator.aktuellesViewModel = new LobbyScreenViewModel(navigator, null);
 
-            }, null);
-        }
+            }, (o) => _selectedIndex != -1);
+            //LobbyBeitreten = new DelegateCommand((o) => { navigator.aktuellesViewModel = new LobbyScreenViewModel(navigator, null); }, (o) => _selectedIndex != -1); //canExcute funktioniert noch nicht
+            }
+        private int _selectedIndex = -1;
+        public int SelectedIndex { get { return _selectedIndex; } set { _selectedIndex = value; this.EigenschaftWurdeGeändert(); this.LobbyBeitreten.RaiseCanExecuteChanged(); } }
+        
+
     }
 }
