15e7ee9b7e7125d87848c1af691d90c770f69cf0
Dominik Mentel <dominik@mentel-web.de>
2023-05-28 15:21:31 +0200

    LobbyErstellenViewModel: Properties sind da und Binding auch. Lobby ErstellenView: Rudimentärer Slider drinnen

--- 03_Implementierung/quaKrypto/quaKrypto/ViewModels/LobbyErstellenViewModel.cs
+++ 03_Implementierung/quaKrypto/quaKrypto/ViewModels/LobbyErstellenViewModel.cs
@@ -28,5 +28,19 @@ namespace quaKrypto.ViewModels
 
             }, null);
         }
+        private string _lobbyName = string.Empty;
+        private int _ausgProtokoll = -1;
+        private int _ausgSchwierigkeit = -1;
+        private int _ausgVariante = -1;
+        private int _ausgPhaseStart;
+        private int _ausgPhaseEnde;
+        private bool _netzwerkbasiert;
+        public string LobbyName { get { return _lobbyName; } set { _lobbyName = value; this.EigenschaftWurdeGeändert(); } }
+        public int AusgProtokoll { get {  return _ausgProtokoll; } set { _ausgProtokoll = value; this.EigenschaftWurdeGeändert(); } }
+        public int AusgSchwierigkeit { get { return _ausgSchwierigkeit; } set { _ausgSchwierigkeit = value; this.EigenschaftWurdeGeändert(); } }
+        public int AusgVariante { get { return _ausgVariante; } set { _ausgVariante = value; this.EigenschaftWurdeGeändert(); } }
+        public int AusgPhaseStart { get { return _ausgPhaseStart; } set { _ausgPhaseStart = value; this.EigenschaftWurdeGeändert(); } }
+        public int AusgPhaseEnd { get { return _ausgPhaseEnde; } set { _ausgPhaseEnde = value; this.EigenschaftWurdeGeändert(); } }
+        public bool NetzwerkBasiert { get { return _netzwerkbasiert; } set { _netzwerkbasiert = value; this.EigenschaftWurdeGeändert();} }
     }
 }
