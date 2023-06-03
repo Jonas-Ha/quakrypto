cd716118ff4d5b52fef37f9dc62a82f89525e62a
Dominik Mentel <dominik@mentel-web.de>
2023-06-03 14:36:40 +0200

    Änderungen an VarianteAbhoeren, etc. LobbyerstellenViewModel, Netzwerk implementiert.

--- 03_Implementierung/quaKrypto/quaKrypto/Models/Classes/UebungsszenarioNetzwerkBeitrittInfo.cs
+++ 03_Implementierung/quaKrypto/quaKrypto/Models/Classes/UebungsszenarioNetzwerkBeitrittInfo.cs
@@ -1,32 +1,41 @@
 ﻿using quaKrypto.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace quaKrypto.Models.Classes
 {
     public class UebungsszenarioNetzwerkBeitrittInfo
     {
-        //TODO Wie mit den Rollen machen?
-        public UebungsszenarioNetzwerkBeitrittInfo(IPAddress address, string lobbyname, string protokoll, string variante, SchwierigkeitsgradEnum schwierigkeitsgrad)
+        public UebungsszenarioNetzwerkBeitrittInfo(IPAddress address, string lobbyname, string protokoll, string variante, SchwierigkeitsgradEnum schwierigkeitsgrad, bool alicestate, bool bobstate, bool evestate)
         {
             IPAddress = address;
             Lobbyname = lobbyname;
             Protokoll = protokoll;
             Variante = variante;
             Schwierigkeitsgrad = schwierigkeitsgrad;
+            AliceState = alicestate;
+            BobState = bobstate;    
+            EveState = evestate;
         }
         public IPAddress IPAddress { get; set; }
         public string Lobbyname { get; set; }
         public string Protokoll { get; set; }
         public string Variante { get; set; }
         public SchwierigkeitsgradEnum Schwierigkeitsgrad { get; set; }
-        public BitmapImage AliceIcon { get; set; }
-        public BitmapImage BobIcon { get; set; }
-        public BitmapImage EveIcon { get; set; }
+        public BitmapImage AliceIcon { get { return new BitmapImage(new Uri(AliceState?"pack://application:,,,/Icons/Spiel/Alice/Alice_128px.png": "pack://application:,,,/Icons/Spiel/Alice/Alice_128px_grey.png")); }}
+        public BitmapImage BobIcon { get { return new BitmapImage(new Uri(BobState? "pack://application:,,,/Icons/Spiel/Bob/Bob_128px.png" : "pack://application:,,,/Icons/Spiel/Bob/Bob_128px_grey.png")); } }
+        public BitmapImage EveIcon { get { return new BitmapImage(new Uri(Variante == VarianteNormalerAblauf.VariantenName? "pack://application:,,,/Icons/Spiel/Eve/Eve_128px_grey.png"/* Durchgestrichene Eve fehlt*/ : EveState? "pack://application:,,,/Icons/Spiel/Eve/Eve_128px.png" : "pack://application:,,,/Icons/Spiel/Eve/Eve_128px_grey.png")); } }
+        public bool AliceState { get; set; }
+        public bool BobState { get; set;}
+        public bool EveState { get; set;}
     }
 }
