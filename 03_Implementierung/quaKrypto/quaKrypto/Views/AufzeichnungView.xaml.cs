33bcdb6ed65ced270e3c3fea69476ce82cfd17a2
Jonas Hammer <jonashammer7@gmail.com>
2023-06-08 14:48:57 +0200

    Aufzeichnung inklusive kleinem Test der Aufzeichnung im ViewModel implementiert

--- 03_Implementierung/quaKrypto/quaKrypto/Views/AufzeichnungView.xaml.cs
+++ 03_Implementierung/quaKrypto/quaKrypto/Views/AufzeichnungView.xaml.cs
@@ -24,5 +24,10 @@ namespace quaKrypto.Views
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
