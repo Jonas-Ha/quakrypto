da22922b297195730ae0f299320113ad9291504f
Alexander Denner <a.denner@oth-aw.de>
2023-06-12 19:55:42 +0200

    InformationsInhaltToString Property Implementiert und getestet

--- 03_Implementierung/quaKrypto/quaKrypto/Models/Classes/Information.cs
+++ 03_Implementierung/quaKrypto/quaKrypto/Models/Classes/Information.cs
@@ -62,16 +62,82 @@ namespace quaKrypto.Models.Classes
             init { informationsEmpfaenger = value; }
         }
 
-        public string InformationsInhaltToString()
+        public string InformationsInhaltToString
+        {
+            get
+            {
+                string erg = string.Empty;
+                if (InformationsInhalt == null) ;
+                else if (InformationsTyp == InformationsEnum.zahl && InformationsInhalt.GetType() == typeof(int))
+                {
+                    erg = ((int)InformationsInhalt).ToString();
+                }
+                else if (InformationsTyp == InformationsEnum.bitfolge && InformationsInhalt.GetType() == typeof(BitArray))
+                {
+                    BitArray bitArray = (BitArray)InformationsInhalt;
+                    for (int i = 0; i < bitArray.Length; i++)
+                    {
+                        erg += bitArray[i] ? 1 : 0;
+                    }
+                }
+                else if (InformationsTyp == InformationsEnum.photonen && InformationsInhalt.GetType() == typeof(byte[]))
+                {
+                    byte[] photonenArray = (byte[])InformationsInhalt;
+                    erg = Photonen_ToString(photonenArray);
+                }
+                else if (InformationsTyp == InformationsEnum.polarisationsschemata && InformationsInhalt.GetType() == typeof(BitArray))
+                {
+                    BitArray bitArray = (BitArray)InformationsInhalt;
+                    for (int i = 0; i < bitArray.Length; i++)
+                    {
+                        erg += bitArray[i] ? '✛' : '✕';
+                    }
+                }
+                else if (InformationsTyp == InformationsEnum.unscharfePhotonen && InformationsInhalt.GetType() == typeof(byte[]))
+                {
+                    byte[] photonenArray = (byte[])InformationsInhalt;
+                    erg = new string('*', photonenArray.Length);
+                }
+                else if (InformationsTyp == InformationsEnum.asciiText && InformationsInhalt.GetType() == typeof(string))
+                {
+                    erg = (string)InformationsInhalt;
+                }
+                else if (InformationsTyp == InformationsEnum.verschluesselterText && InformationsInhalt.GetType() == typeof(string))
+                {
+                    erg = (string)InformationsInhalt;
+                }
+                return erg;
+            }
+        }
+
+        private string Photonen_ToString(byte[] photonenArray)
         {
             string erg = string.Empty;
-            if (InformationsInhalt == null) ;
-            else if (InformationsTyp == InformationsEnum.bitfolge && InformationsInhalt.GetType() == typeof(BitArray))
+            for (int i = 0; i < photonenArray.Length; i++)
             {
-                BitArray bitArray = (BitArray)InformationsInhalt;
-                for(int i = 0; i < bitArray.Length; i++)
+                //21 (Photonen)
+                //XX 1.Bit = Polarisationsschmata, 2.Bit = Schlüssel
+                if ((photonenArray[i] & 0x01) == 1)
                 {
-                    erg += bitArray[i] ? 1 : 0;
+                    if ((photonenArray[i] & 0x02) == 2)
+                    {
+                        erg += '─'; //0x03
+                    }
+                    else
+                    {
+                        erg += '│'; //0x01
+                    }
+                }
+                else
+                {
+                    if ((photonenArray[i] & 0x02) == 2)
+                    {
+                        erg += '╱'; //0x02
+                    }
+                    else
+                    {
+                        erg += '╲'; //0x00
+                    }
                 }
             }
             return erg;
